76a3f00de133ac3ac87baafe07fc2c64d00df9ca
andreybozhkov <bozhkov.andrey@gmail.com>
2020-02-11 21:26:00 +0200

    Added initial line generation

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const fs = require('fs');
 const pdf = require('pdf-parse');
-//let regexp = /(2020\d{5})|(204\d{4})|(\b\d{1,6}\.\d{2}\b)/g;
 let regexRef = '\\b2020\\d{5}\\b';
 let regexProjNum = '\\b204\\d{4}\\b';
-let regexCostType = '\\b[A-Z]{3}\\b';
+//let regexCostType = '\\b[A-Z]{3}\\b';
 let regexNum = '\\b\\d{1,6}\\.\\d{2}\\b';
-let regexpAll = regexRef + '|' + regexProjNum + '|' + regexCostType + '|' + regexNum;
+let regexpAll = regexRef + '|' + regexProjNum + '|' + regexNum;
 let regexp = new RegExp(regexpAll, 'g')
 
-let dataBuffer = fs.readFileSync('./data/3_invoice.pdf');
+let dataBuffer = fs.readFileSync('./data/10_v2invoice.pdf');
 
 pdf(dataBuffer).then(function(data) {
     let matches = data.text.match(regexp);
@@ -16,4 +15,32 @@ pdf(dataBuffer).then(function(data) {
         if (err) throw err;
         console.log('Match file written.');
     });
+    
+    let lines = [];
+
+    for (let i = 0; i < matches.length; i++) {
+        if (new RegExp(regexNum).test(matches[i])) {
+            let amount = Number(matches[i]);
+            let projectNum = '';
+            if (new RegExp(regexProjNum).test(matches[i - 1])) {
+                projectNum = matches[i - 1];
+            }
+            let ref = '';
+            if (new RegExp(regexRef).test(matches[i - 2])) {
+                ref = matches[i - 2];
+            }
+            let newLine = new singleLine(projectNum, ref);
+            newLine.addAmount(amount);
+            console.log(newLine);
+        }
+    }
 });
+
+function singleLine(projNum, ref) {
+    this.summedAmount = 0;
+    this.projNum = projNum;
+    this.supplierRef = ref;
+    this.addAmount = (amount) => {
+        this.summedAmount += amount;
+    };
+}
