195221cb53af29dc97f1c00d13e8f45f7d5eb1e4
andreybozhkov <bozhkov.andrey@gmail.com>
2020-02-11 19:29:59 +0200

    Update regex construction methodology

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const fs = require('fs');
 const pdf = require('pdf-parse');
 //let regexp = /(2020\d{5})|(204\d{4})|(\b\d{1,6}\.\d{2}\b)/g;
-let regexp = /2020\d{5}|20\d{5}|\b[A-Z]{3}\b|\b\d{1,6}\.\d{2}\b/g;
+let regexRef = '\\b2020\\d{5}\\b';
+let regexProjNum = '\\b204\\d{4}\\b';
+let regexCostType = '\\b[A-Z]{3}\\b';
+let regexNum = '\\b\\d{1,6}\\.\\d{2}\\b';
+let regexpAll = regexRef + '|' + regexProjNum + '|' + regexCostType + '|' + regexNum;
+let regexp = new RegExp(regexpAll, 'g')
 
 let dataBuffer = fs.readFileSync('./data/3_invoice.pdf');
 
-let matches = [];
 pdf(dataBuffer).then(function(data) {
-    matches = [...data.text.matchAll(regexp)];
+    let matches = data.text.match(regexp);
     fs.writeFile('./data/matches.txt', matches, (err) => {
         if (err) throw err;
         console.log('Match file written.');
