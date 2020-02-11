81c242586d6c8f3535f425086eea2e83771e300c
andreybozhkov <bozhkov.andrey@gmail.com>
2020-02-11 17:41:02 +0200

    Updated matching method

--- index.js
+++ index.js
@@ -1,53 +1,15 @@
 const fs = require('fs');
 const pdf = require('pdf-parse');
-//let regexp = /(2020\d{5})(.*)(\b\d{1,6}\.\d{2}\b)\n/g;
-let regexp = /2020\d{5}/g;
+//let regexp = /(2020\d{5})|(204\d{4})|(\b\d{1,6}\.\d{2}\b)/g;
+let regexp = /2020\d{5}|20\d{5}|\b[A-Z]{3}\b|\b\d{1,6}\.\d{2}\b/g;
 
-let dataBuffer = fs.readFileSync('./data/invoice.pdf');
+let dataBuffer = fs.readFileSync('./data/3_invoice.pdf');
 
-// default render callback
-function render_page(pageData) {
-    //check documents https://mozilla.github.io/pdf.js/
-    let render_options = {
-        //replaces all occurrences of whitespace with standard spaces (0x20). The default value is `false`.
-        normalizeWhitespace: false,
-        //do not attempt to combine same line TextItem's. The default value is `false`.
-        disableCombineTextItems: false
-    }
- 
-    return pageData.getTextContent(render_options)
-    .then(function(textContent) {
-        let lastY, text = '';
-        for (let item of textContent.items) {
-            if (lastY == item.transform[5] || !lastY){
-                text += item.str;
-            }  
-            else{
-                text += '\n' + item.str;
-            }    
-            lastY = item.transform[5];
-        }
-        return text;
+let matches = [];
+pdf(dataBuffer).then(function(data) {
+    matches = [...data.text.matchAll(regexp)];
+    fs.writeFile('./data/matches.txt', matches, (err) => {
+        if (err) throw err;
+        console.log('Match file written.');
     });
-}
- 
-let options = {
-    pagerender: render_page
-}
-
-pdf(dataBuffer, options).then(function(data) {
-    // PDF text
-    //console.log(data.text);
-    //console.log(data.text.toString());
-    //let stringified = data.text.toString();
-    //console.log(stringified);
-    //console.log(data.text);
-    let matches = [...data.text.matchAll(regexp)];
-    //console.log(matches);
-    //console.log(matches);
-    //let matches = getMatches(data.text.toString(), regExp);
-    //console.log(matches);
-    for (let m of matches) {
-        console.log(m[0]);
-    }
 });
